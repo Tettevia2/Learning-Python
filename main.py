bf2c78f940380c77bd3e783d0c81d3c05fd5a073
Tettevia2 <tetteviasonidhisefakor@gmail.com>
2022-07-14 17:01:09 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+name =input( "What is your name?")
+Age = input("What is your age? ")
+int(Age)
+cal= 2022 - int(Age)
+first= "Hello " + name + " you were born in"
+def getyear(Ages):
+     print(first, + Ages  )
+
+getyear(cal)
